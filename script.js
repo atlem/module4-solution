a32b840e52f9576f9c6d74253dc6b5818e27811e
atlem <atle.mjolhus@live.no>
2025-02-14 22:49:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 (function () {
+  // List of names to greet
   var names = [
     "Yaakov", "John", "Jen", "Jason", 
     "Paul", "Frank", "Larry", "Paula", 
     "Laura", "Jim"
   ];
+
+  // Loop over each name and determine the greeting
   for (var i = 0; i < names.length; i++) {
+    // Retrieve the first letter and convert to lowercase
     var firstLetter = names[i].charAt(0).toLowerCase();
+
+    // If the name starts with 'j', say "Good Bye", otherwise "Hello"
     if (firstLetter === 'j') {
       byeSpeaker.speak(names[i]);
     } else {
