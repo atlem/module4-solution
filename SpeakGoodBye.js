dda44a14b9f502ea1ef6b07d651fff52e213a724
atlem <atle.mjolhus@live.no>
2025-02-14 22:51:22 +0100

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -1,8 +1,22 @@
+// Wrap the entire contents inside an IIFE
 (function (window) {
   var byeSpeaker = {};
   var speakWord = "Good Bye";
+
   byeSpeaker.speak = function(name) {
-    console.log(speakWord + " " + name);
+    var message = speakWord + " " + name;
+    // Log to console
+    console.log(message);
+    // Append to HTML output
+    var outputDiv = document.getElementById("output");
+    if (outputDiv) {
+      var p = document.createElement("p");
+      p.textContent = message;
+      outputDiv.appendChild(p);
+    }
   };
+
+  // Expose byeSpeaker to the global object
   window.byeSpeaker = byeSpeaker;
+
 })(window);
